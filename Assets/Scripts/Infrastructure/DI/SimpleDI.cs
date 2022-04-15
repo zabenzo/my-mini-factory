f736eb0152a95ceb6b000ee6e977e2e1dcdc2fb2
Uladzislau Daroshka <benzolad@icloud.com>
2022-04-15 14:05:34 +0300

    Fix DI conflicts

--- Assets/Scripts/Infrastructure/DI/SimpleDI.cs
+++ Assets/Scripts/Infrastructure/DI/SimpleDI.cs
@@ -1,23 +1,21 @@
+using Infrastructure.Services;
+
 namespace Infrastructure.DI
 {
     public class SimpleDI
     {
-        public class ServiceLocator
+        private static SimpleDI _instance;
+        public static SimpleDI Container => _instance ??= new SimpleDI();
+
+        public void RegisterSingle<TService>(TService implementation) where TService : IService => 
+            Implementation<TService>.ServiceInstance = implementation;
+
+        public TService Single<TService>() where TService : IService => 
+            Implementation<TService>.ServiceInstance;
+
+        private static class Implementation<TService> where TService : IService
         {
-            private static ServiceLocator _instance;
-            public static ServiceLocator Container => _instance ??= new ServiceLocator();
-
-            public void RegisterSingle<TService>(TService implementation) where TService : IService => 
-                Implementation<TService>.ServiceInstance = implementation;
-
-            public TService Single<TService>() where TService : IService => 
-                Implementation<TService>.ServiceInstance;
-
-            private static class Implementation<TService> where TService : IService
-            {
-                public static TService ServiceInstance;
-            }
-
+            public static TService ServiceInstance;
         }
     }
 }
